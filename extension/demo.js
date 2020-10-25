f9133781bd23b6af462bd4414b55e08067973cb1
Julien Nury <jnury@users.noreply.github.com>
2020-10-25 22:39:25 +0100

    Bootstrap version + custom API call

--- extension/demo.js
+++ extension/demo.js
@@ -6,105 +6,183 @@
 
 var encodedChallenge = '';
 var challengeResponse = '';
-let apiKeyField = document.getElementById('apiKey');
-let requestChallenge = document.getElementById('requestChallenge');
-let generateDeviceResponse = document.getElementById('generateDeviceResponse');
-let generateUserResponse = document.getElementById('generateUserResponse');
-let verifyChallengeResponse = document.getElementById('verifyChallengeResponse');
+var simulation = false;
+var challengeType = $("input[name='challengeType']:checked").val();
 
-// Load API Key from local storage
-chrome.storage.sync.get('apiKey', function(data) {
-    if (data) {
-        apiKeyField.value = data.apiKey;
-    }
-});
-
-// Save the API Key to local storage (if changed)
-apiKeyField.onchange = function(element) {
-    chrome.storage.sync.set({apiKey: apiKeyField.value}, function() {
-        console.log('API Key saved to local storage');
-    });
-};
-
-// Request challenge from URL
-requestChallenge.onclick = function(element) {
-    let apiKey = apiKeyField.value;
-    if (apiKey != '') {
-        let challengeUrlString = 'https://verifiedaccess.googleapis.com/v1/challenge?key=' + apiKey;
-        $('#encodedChallenge').empty().append('Requesting challenge ...');
-        let xmlhttp = new XMLHttpRequest();
-        xmlhttp.open('POST', challengeUrlString, true);
-        xmlhttp.send();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4) {
-                var challenge = xmlhttp.responseText;
-                encodedChallenge = encodeChallenge(challenge);
-                $('#encodedChallenge').empty().append('Encoded Challenge: ' + encodedChallenge);
-            }
-        };
-    }
-    else {
-        $('#encodedChallenge').empty().append('ERROR: please enter your Google API key in the form above');
-    }
-    
-};
-
-// Generate challenge response
-var generateResponse = function(isDeviceVerification) {
-    if (encodedChallenge != '') {
-        if (testPlatformKeysAvailability()) {
-            try {
-                if (isDeviceVerification) {
-                    $('#challengeResponse').empty().append('Generating Device response ...');
-                    chrome.enterprise.platformKeys.challengeMachineKey(
-                        decodestr2ab(encodedChallenge), true, ChallengeCallback);
-                } else {
-                    $('#challengeResponse').empty().append('Generating User response ...');
-                    chrome.enterprise.platformKeys.challengeUserKey(
-                        decodestr2ab(encodedChallenge), true, ChallengeCallback);
-                }
-            } catch (error) {
-                $('#challengeResponse').empty().append('ERROR: ' + error);
-            }
-        }
-        else {
-            $('#challengeResponse').empty().append('ERROR: TPM API not available. Please check prerequisites');
-        }
-    }
-    else {
-        $('#challengeResponse').empty().append('ERROR: please request a challenge first');
-    }
+// Trap errors
+window.onerror = function myErrorHandler(errorMsg, url, lineNumber) {
+    let message = '<pre>An error occured in: ' + url + '<br>On line: ' + lineNumber + '<br>Message: ' + errorMsg + '</pre>'
+    $('#errorModalMessage').empty().append(message);
+    $('#errorModal').modal('show')
+    return false;
 }
 
-// Generate challenge events
-generateDeviceResponse.onclick = function(element) { generateResponse(true); };
-generateUserResponse.onclick = function(element) { generateResponse(false); };
+// Callback to retrieve the user identity
+var GetIdentityCallback = function(userInfo) {
+    let email = 'user@domain.com'
+    if (userInfo.email) {
+        email = userInfo.email
+    }
 
-// Callback to retreive the challenge response
+    if (challengeType == 'device') {
+        let lasta = email.lastIndexOf('@');
+        if (lasta != -1) {
+            $('#expectedIdentity').val(email.substring(lasta+1))
+        } else {
+            $('#expectedIdentity').val('domain.com')
+        }
+    }
+    else {
+        $('#expectedIdentity').val(email)
+    }
+};
+
+// Callback to retrieve the challenge response
 var ChallengeCallback = function(response) {
     if (chrome.extension.lastError) {
         $('#challengeResponse').empty().append('ERROR: ' + chrome.extension.lastError.message);
     } else {
         var responseAsString = ab2base64str(response);
         challengeResponse = responseAsString;
-        $('#challengeResponse').empty().append('Challenge response: ' + responseAsString);
+        $('#challengeResponse').empty().append(responseAsString);
     }
 };
 
-// Verify challenge response
-verifyChallengeResponse.onclick = function(element) {
-    let expectedIdentity = document.getElementById('expectedIdentity');
-    if (expectedIdentity.value != '') {
-        if (challengeResponse != '') {
-            let apiKey = document.getElementById('apiKey').value;
-            let challengeUrlString = 'https://verifiedaccess.googleapis.com/v1/challenge:verify?key=' + apiKey;
-            $('#verifyResult').empty().append('Verifying challenge response ...');
-        }
-        else {
-            $('#verifyResult').empty().append('ERROR: please generate a challenge response first');
+// Toggle to simulation mode if enterprise API is not available
+if (!testPlatformKeysAvailability()) {
+    $('#apiWarning').collapse('show');
+    simulation = true;
+}
+
+// Init Expected identity field
+chrome.identity.getProfileUserInfo(GetIdentityCallback);
+
+// Load API Key from local storage
+chrome.storage.sync.get('apiKey', function(data) {
+    if (data) {
+        $('#apiKey').val(data.apiKey)
+    }
+});
+
+// Save the API Key to local storage (if changed)
+$('#apiKey').on('input', function() {
+    chrome.storage.sync.set({apiKey: $('#apiKey').val()}, function() {
+        console.log('API Key saved to local storage');
+    });
+});
+
+// Load custom API URL from local storage
+chrome.storage.sync.get('customApiUrl', function(data) {
+    if (data) {
+        $('#customApiUrl').val(data.customApiUrl)
+    }
+});
+
+// Save the custom API URL to local storage (if changed)
+$('#customApiUrl').on('input', function() {
+    chrome.storage.sync.set({customApiUrl: $('#customApiUrl').val()}, function() {
+        console.log('Custom API URL saved to local storage');
+    });
+});
+
+// Update challenge type (when changed)
+$("input[name='challengeType']").change(function(element) {
+    challengeType = $("input[name='challengeType']:checked").val();
+    chrome.identity.getProfileUserInfo(GetIdentityCallback);
+});
+
+// Request challenge from Google API
+$('#requestChallenge').click(function(element) {
+    let apiKey = $('#apiKey').val();
+    $('#encodedChallenge').collapse('show');
+    if (apiKey) {
+        try {
+            let challengeUrlString = 'https://verifiedaccess.googleapis.com/v1/challenge?key=' + apiKey;
+            $('#encodedChallenge').empty().append('Requesting challenge ...');
+            let xmlhttp = new XMLHttpRequest();
+            xmlhttp.open('POST', challengeUrlString, true);
+            xmlhttp.send();
+            xmlhttp.onreadystatechange = function() {
+                if (xmlhttp.readyState === 4) {
+                    if (xmlhttp.status === 200) {
+                        var challenge = xmlhttp.responseText;
+                        encodedChallenge = encodeChallenge(challenge);
+                        $('#encodedChallenge').empty().append(encodedChallenge);
+                    } else {
+                        $('#encodedChallenge').empty().append('ERROR: ' + xmlhttp.responseText);
+                    }
+                }
+            };
+        } catch (error) {
+            $('#encodedChallenge').empty().append('ERROR: ' + error);
         }
     }
     else {
-        $('#verifyResult').empty().append('ERROR: please enter an expected identity (domain for device or username for user)');
+        $('#encodedChallenge').empty().append('ERROR: please enter your Google API Key (step 1)');
     }
-}
+});
+
+// Generate challenge response
+$('#generateResponse').click(function(element) {
+    $('#challengeResponse').collapse('show');
+    if (encodedChallenge != '') {
+        try {
+            if (challengeType == 'device') {
+                if (simulation) {
+                    challengeResponse = 'A_Fake_Device_Response'
+                    $('#challengeResponse').empty().append(challengeResponse);
+                } else {
+                    $('#challengeResponse').empty().append('Generating Device response ...');
+                    chrome.enterprise.platformKeys.challengeMachineKey(
+                        decodestr2ab(encodedChallenge), true, ChallengeCallback);
+                }
+            } else {
+                if (simulation) {
+                    challengeResponse = 'A_Fake_User_Response'
+                    $('#challengeResponse').empty().append(challengeResponse);
+                } else {
+                    $('#challengeResponse').empty().append('Generating User response ...');
+                    chrome.enterprise.platformKeys.challengeUserKey(
+                        decodestr2ab(encodedChallenge), true, ChallengeCallback);
+                }
+            }
+        } catch (error) {
+            $('#challengeResponse').empty().append('ERROR: ' + error);
+        }
+    }
+    else {
+        $('#challengeResponse').empty().append('ERROR: please request a challenge first (step 2)');
+    }
+});
+
+// CAll remote API
+$('#sendResponse').click(function(element) {
+    $('#remoteStatus').collapse('show');
+    let customApiUrl = $('#customApiUrl').val();
+    if (customApiUrl) {
+        let expectedIdentity = $('#expectedIdentity').val();
+        if (expectedIdentity) {
+            if (challengeResponse != '') {
+                $('#remoteStatus').empty().append('Calling ' + customApiUrl + ' ...');
+                $.ajax({
+                    type: "POST",
+                    url: customApiUrl,
+                    data: JSON.stringify({ "expectedIdentity": expectedIdentity, "challengeResponse": challengeResponse}),
+                    contentType : 'application/json',
+                    dataType: "text",
+                    success: function(data, status, xhr) { $('#remoteStatus').empty().append(data); },
+                    error: function(request, status, error) { $('#remoteStatus').empty().append('ERROR: ' + request.responseText); }
+                });
+            }
+            else {
+                $('#remoteStatus').empty().append('ERROR: please generate a challenge response first (step 3)');
+            }
+        }
+        else {
+            $('#remoteStatus').empty().append('ERROR: please enter the expected identity (step 4)');
+        }
+    }
+    else {
+        $('#remoteStatus').empty().append('ERROR: please provide your custom API url (step 5)');
+    }
+});
